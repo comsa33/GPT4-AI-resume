dd07f59397679c28036b0348b025f49f398f2177
ruo <ruo@gravylab.co.kr>
2023-03-23 14:40:35 +0900

    fix: functions.py

--- core/functions.py
+++ core/functions.py
@@ -1,13 +1,15 @@
 import pandas as pd
+from sqlalchemy import text
 
 import data.queries as nq
 from data.postgre import postgre_engine_ailab as engine_ailab
 
 table_names = ["wanted"]
 
+
 def get_data(table_name):
     if table_name == "wanted":
         query = nq.FindAllFromWantedJobposting
     with engine_ailab.connect() as conn:
-        fetch = conn.execute(query).fetchall()
+        fetch = conn.execute(text(query)).fetchall()
     return pd.DataFrame(fetch)
